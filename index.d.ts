35ac8df717f7041ac0e1b09dcbaa4856b5b75bcb
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-20 10:25:12 +0500

    add JsDoc

--- index.d.ts
+++ index.d.ts
@@ -3,45 +3,89 @@ import { ViewStyle } from "react-native";
 
 type ButtonStyleType = {
   [name: string]: {
-      backgroundColor: string,
-      textColor: string,
-      borderColor: string,
-      borderWidth: number,
-      borderRadius: number
-  }
+    backgroundColor: string;
+    textColor: string;
+    borderColor: string;
+    borderWidth: number;
+    borderRadius: number;
+  };
 };
 
 type BackgroundStyleType = {
   [name: string]: {
-    backgroundColor: string,
-    borderColor: string,
-    borderWidth: number,
-    borderRadius: number
-  }
+    backgroundColor: string;
+    borderColor: string;
+    borderWidth: number;
+    borderRadius: number;
+  };
 };
 
 declare module "react-native-admob-native-ads" {
   export type NativeAdViewProps = {
-    style?:ViewStyle,
-    adSize?: string,
-    adUnitID: string,
-    buttonStyle?: ButtonStyleType,
-    backgroundStyle?:BackgroundStyleType,
-    headlineTextColor?:string,
-    descriptionTextColor?:string,
-    advertiserTextColor?:string,
-    ratingBarColor?:string,
-    testDevices?: Array<string>,
-    onAdOpened?: Function<void>,
-    onAdClosed?: Function<void>,
-    onAdLeftApplication?: Function<void>,
-    onAdImpression?: Function<void>,
-    onAdClick?: Function<void>,
-    onAdLoaded?: Function<void>,
-    onAdFailedToLoad?: Function<void>,
+    style?: ViewStyle;
+
+    /**
+     * adSize: Size of the ad that you want to display.
+     *
+     * Android: small, medium, large
+     *
+     * iOS: small & medium only
+     *
+     */
+    adSize?: string;
+
+    /**
+     * adUnitID: Set Ad Unit ID for Native Advanced Ads that you created on your AdMob account.
+     */
+
+    adUnitID: string;
+
+    /**
+     * buttonStyle: style the callToAction button in Native ad according to your app look and feel.
+     *
+     */
+
+    buttonStyle?: ButtonStyleType;
+
+    /**
+     * backgroundStyle: Style the background of Native Ad View.
+     */
+
+    backgroundStyle?: BackgroundStyleType;
+
+    /**
+     * Set color for the heading text of Ad.
+     */
+
+    headlineTextColor?: string;
+
+    /**
+     * Set color for the description text of Ad.
+     */
+    descriptionTextColor?: string;
+    /**
+     * Set color for the advertiser text of Ad.
+     */
+    advertiserTextColor?: string;
+    /**
+     * Set color for the rating stars of Ad.
+     * Android Only.
+     */
+    ratingBarColor?: string;
+
+    /**
+     * Set testdevices for the ad. 
+     */
+
+    testDevices?: Array<string>;
+    onAdOpened?: Function<void>;
+    onAdClosed?: Function<void>;
+    onAdLeftApplication?: Function<void>;
+    onAdImpression?: Function<void>;
+    onAdClick?: Function<void>;
+    onAdLoaded?: Function<void>;
+    onAdFailedToLoad?: Function<void>;
   };
 
-  export default class NativeAdView extends Component<NativeAdViewProps> {
-   
-  }
+  export default class NativeAdView extends Component<NativeAdViewProps> {}
 }
