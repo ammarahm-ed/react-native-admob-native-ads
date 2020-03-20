dd85e082c675f27c79155e049c4f26b7704baa71
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-20 11:03:48 +0500

    init example project

---
+++ example/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+
+} from 'react-native';
+
+
+
+const App = () => {
+
+
+  
+
+  return (
+    <>
+      <SafeAreaView
+        style={{
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+         
+          
+
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
