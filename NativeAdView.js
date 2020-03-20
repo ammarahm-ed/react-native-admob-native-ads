97ba723f6cce36b37adaa2641c77d8a4133148e9
ammarahm-ed <ammarahmed6506@gmail.com>
2020-03-20 10:29:01 +0500

    cleanup imports

--- NativeAdView.js
+++ NativeAdView.js
@@ -1,34 +1,33 @@
-import { arrayOf, func, string, bool, objectOf, object } from 'prop-types';
-import React, { Component } from 'react';
+import { arrayOf, func, object, string } from "prop-types";
+import React, { Component } from "react";
 import {
   findNodeHandle,
   requireNativeComponent,
   UIManager,
-  ViewPropTypes,
-} from 'react-native';
-import { createErrorFromErrorData } from './utils';
+  ViewPropTypes
+} from "react-native";
 
 class NativeAdView extends Component {
   constructor() {
     super();
     this.handleAdFailedToLoad = this.handleAdFailedToLoad.bind(this);
     this.state = {
-      style: {},
+      style: {}
     };
   }
 
   componentDidMount() {
-   const { adUnitID } = this.props;
-   if (adUnitID) {
-     return this.loadNativeAd(adUnitID);
+    const { adUnitID } = this.props;
+    if (adUnitID) {
+      return this.loadNativeAd(adUnitID);
     }
-    console.warn('Attempted to load native ad without ad unit id');
+    console.warn("Attempted to load native ad without ad unit id");
   }
 
   loadNativeAd(adUnitId) {
     UIManager.dispatchViewManagerCommand(
       findNodeHandle(this._nativeView),
-      UIManager.getViewManagerConfig('RNGADNativeView').Commands.loadNativeAd,
+      UIManager.getViewManagerConfig("RNGADNativeView").Commands.loadNativeAd,
       [adUnitId]
     );
   }
@@ -39,7 +38,7 @@ class NativeAdView extends Component {
     }
   }
 
-  setRef = (el) => (this._nativeView = el);
+  setRef = el => (this._nativeView = el);
 
   render() {
     return (
@@ -53,18 +52,18 @@ class NativeAdView extends Component {
   }
 }
 
-NativeAdView.simulatorId = 'SIMULATOR';
+NativeAdView.simulatorId = "SIMULATOR";
 
 NativeAdView.propTypes = {
   ...ViewPropTypes,
   adSize: string,
   adUnitID: string,
   buttonStyle: object,
-  backgroundStyle:object,
-  headlineTextColor:string,
-  descriptionTextColor:string,
-  advertiserTextColor:string,
-  ratingBarColor:string,
+  backgroundStyle: object,
+  headlineTextColor: string,
+  descriptionTextColor: string,
+  advertiserTextColor: string,
+  ratingBarColor: string,
   testDevices: arrayOf(string),
   onAdOpened: func,
   onAdClosed: func,
@@ -72,9 +71,12 @@ NativeAdView.propTypes = {
   onAdImpression: func,
   onAdClick: func,
   onAdLoaded: func,
-  onAdFailedToLoad: func,
+  onAdFailedToLoad: func
 };
 
-const UnifiedNativeAdView = requireNativeComponent('RNGADNativeView', NativeAdView);
+const UnifiedNativeAdView = requireNativeComponent(
+  "RNGADNativeView",
+  NativeAdView
+);
 
 export default NativeAdView;
