816b341f84a3ca20cf046197f008f8c13ddd4ab1
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-13 21:57:56 +0500

    export all Views

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import IconView from './src/IconView';
+import CallToActionView from './src/CallToActionView'
+import HeadlineView from './src/HeadlineView'
+import TaglineView from './src/TaglineView'
+import AdvertiserView from './src/AdvertiserView'
+import AdChoicesView from './src/AdChoicesView'
+import ImageView from './src/ImageView'
+import MediaView from './src/MediaView'
+import StoreView from './src/StoreView'
+import StarRatingView from './src/StarRatingView'
 import NativeAdView from './src';
-
-
-
 
 export {
   IconView,
-  NativeAdView
+  NativeAdView,
+  CallToActionView,
+  HeadlineView,
+  TaglineView,
+  AdChoicesView,
+  AdvertiserView,
+  ImageView,
+  MediaView,
+  StoreView,
+  StarRatingView
 }
 
 
