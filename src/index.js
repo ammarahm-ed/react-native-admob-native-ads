2c8183487c95f107677e496d4a403b4557f7f455
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-13 20:53:17 +0500

    convert to functional component & add context

--- src/index.js
+++ src/index.js
@@ -1,66 +1,43 @@
-import { arrayOf, func, object, string } from "prop-types";
-import React, { Component, createRef } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import {
   findNodeHandle,
   requireNativeComponent,
   UIManager,
-  ViewPropTypes,
 } from "react-native";
+import { NativeAdContext } from "./context";
 
 const nativeAdView = createRef();
 
-class NativeAdView extends Component {
-  constructor() {
-    super();
-  }
-
-  componentDidMount() {
-    const { adUnitID } = this.props;
-    if (adUnitID) {
-      return this.loadNativeAd(adUnitID);
+const NativeAdView = (props) => {
+  const [nativeAd, setNativeAd] = useState(null);
+  
+  function updateAd(ad) {
+    if (ad) {
+      setNativeAd(ad)
     }
-    console.warn("Attempted to load native ad without ad unit id");
   }
 
-  loadNativeAd(adUnitId) {
-    UIManager.dispatchViewManagerCommand(
-      findNodeHandle(nativeAdView),
-      UIManager.getViewManagerConfig("RNGADNativeView").Commands.loadNativeAd,
-      [adUnitId]
-    );
-  }
-
-  render() {
-    return (
+  return (
+    <NativeAdContext.Provider
+      value={{ nativeAd, setNativeAd }}
+    >
       <UnifiedNativeAdView
-        {...this.props}
+        {...props}
         ref={nativeAdView}
+        onUnifiedNativeAdLoaded={event => {
+          updateAd(event.nativeEvent)
+          if (this.props.onUnifiedNativeAdLoaded) {
+            props.onUnifiedNativeAdLoaded(event);
+          }
+        }}
       />
-    );
-  }
+    </NativeAdContext.Provider>
+  );
+
 }
 
 NativeAdView.simulatorId = "SIMULATOR";
 
-NativeAdView.propTypes = {
-  ...ViewPropTypes,
-  adSize: string,
-  adUnitID: string,
-  buttonStyle: object,
-  backgroundStyle: object,
-  headlineTextColor: string,
-  descriptionTextColor: string,
-  advertiserTextColor: string,
-  ratingBarColor: string,
-  testDevices: arrayOf(string),
-  onAdOpened: func,
-  onAdClosed: func,
-  onAdLeftApplication: func,
-  onAdImpression: func,
-  onAdClick: func,
-  onAdLoaded: func,
-  onAdFailedToLoad: func
-};
 
 const UnifiedNativeAdView = requireNativeComponent(
   "RNGADNativeView",
