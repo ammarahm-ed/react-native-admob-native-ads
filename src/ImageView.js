2777c49d358643a6ee610d46eff2e0dc8befa405
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-16 10:37:05 +0500

    remove unnecessary nesting

--- src/ImageView.js
+++ src/ImageView.js
@@ -1,22 +1,19 @@
 
 import React, { useContext } from "react";
 import {
-  View,
   Image,
 } from "react-native";
 import { NativeAdContext } from "./context";
 
 const ImageView = (props) => {
   const { nativeAd, setNativeAd } = useContext(NativeAdContext);
-  return (<View
-    style={props.style}
-    nativeID="adImageView"
-  >
+  return (
     <Image
       style={props.imageStyle}
+      nativeID="adImageView"
       source={{ uri: nativeAd? nativeAd.images[0].uri : null }}
     />
-  </View>);
+ );
 }
 
 export default ImageView;
