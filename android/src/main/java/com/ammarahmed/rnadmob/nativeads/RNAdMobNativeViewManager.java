7beeabe9fd06f6d35880bf02876a868b21a01c52
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-16 19:40:34 +0500

    refactor

--- android/src/main/java/com/ammarahmed/rnadmob/nativeads/RNAdMobNativeViewManager.java
+++ android/src/main/java/com/ammarahmed/rnadmob/nativeads/RNAdMobNativeViewManager.java
@@ -1,17 +1,15 @@
 package com.ammarahmed.rnadmob.nativeads;
 
 import android.view.View;
+
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableNativeArray;
-
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.annotations.ReactProp;
-
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.RequestConfiguration;
-import com.google.android.gms.ads.formats.UnifiedNativeAd;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,12 +18,10 @@ import java.util.Map;
 
 
 public class RNAdMobNativeViewManager extends ViewGroupManager<RNNativeAdWrapper> {
-    private static final String TAG = "RNAdMobNativeViewManag";
+
+
     public static final String REACT_CLASS = "RNGADNativeView";
-    protected UnifiedNativeAd unifiedNativeAd;
     private RNNativeAdWrapper nativeAdView;
-    private ThemedReactContext mContext;
-    private String admobAdUnitId = "";
 
     public static final String EVENT_AD_FAILED_TO_LOAD = "onAdFailedToLoad";
     public static final String EVENT_AD_CLICKED = "onAdClicked";
@@ -36,79 +32,11 @@ public class RNAdMobNativeViewManager extends ViewGroupManager<RNNativeAdWrapper
     public static final String EVENT_AD_LEFT_APPLICATION = "onAdLeftApplication";
     public static final String EVENT_UNIFIED_NATIVE_AD_LOADED = "onUnifiedNativeAdLoaded";
 
-    public String adHeadline = "adHeadlineView";
-    public String adTagline = "adTaglineView";
-    public String adAdvertiser = "adAdvertiserView";
-    public String adStarRating = "adStarRating";
-    public String adImageView = "adImageView";
-    public String adIconView = "adIconView";
-    public String adCallToAction = "adCallToAction";
-    public String adStoreView = "adStoreView";
-    public int AdChoicesViewId = 733;
-
-
-
     public static final String PROP_TEST_DEVICES = "testDevices";
     public static final String PROP_AD_UNIT_ID = "adUnitID";
+    public static final String PROP_MEDIA_VIEW = "adMediaView";
+    public static final String PROP_REFRESH_INTERVAL = "refreshInterval";
 
-
-    @Override
-    public String getName() {
-        return REACT_CLASS;
-    }
-
-    @Override
-    protected RNNativeAdWrapper createViewInstance(ThemedReactContext reactContext) {
-        mContext = reactContext;
-        nativeAdView = new RNNativeAdWrapper(reactContext);
-
-        return nativeAdView;
-    }
-
-    @Override
-    public void addView(RNNativeAdWrapper parent, View child, int index) {
-        //super.addView(parent, child, index);
-
-        nativeAdView.addNewView(child,index);
-
-
-    }
-
-    /*
-
-
-    @ReactProp(name = "adChoicesView")
-    public void setAdChoicesView(final RNNativeAdWrapper view, final int adChoicesViewId) {
-
-        AdChoicesView adChoicesView = nativeAdView.findViewById(adChoicesViewId);
-        if (adChoicesView != null)
-           // nativeAdView.setAdChoicesView(adChoicesView);
-
-
-    }
-
-     */
-
-    @ReactProp(name = "adMediaView")
-    public void setMediaView(final RNNativeAdWrapper view, final int mediaViewId) {
-
-
-
-       nativeAdView.addMediaView(mediaViewId);
-
-
-    }
-
-    @ReactProp(name = PROP_TEST_DEVICES)
-    public void setPropTestDevices(final RNNativeAdWrapper view, final ReadableArray testDevices) {
-        ReadableNativeArray nativeArray = (ReadableNativeArray) testDevices;
-        ArrayList<Object> list = nativeArray.toArrayList();
-
-        List<String> testDeviceIds = Arrays.asList(list.toArray(new String[list.size()]));
-        RequestConfiguration configuration =
-                new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
-        MobileAds.setRequestConfiguration(configuration);
-    }
 
     @javax.annotation.Nullable
     @Override
@@ -130,10 +58,55 @@ public class RNAdMobNativeViewManager extends ViewGroupManager<RNNativeAdWrapper
         return builder.build();
     }
 
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    protected RNNativeAdWrapper createViewInstance(ThemedReactContext reactContext) {
+        nativeAdView = new RNNativeAdWrapper(reactContext);
+        return nativeAdView;
+    }
+
+    @Override
+    public void addView(RNNativeAdWrapper parent, View child, int index) {
+        //super.addView(parent, child, index);
+
+        nativeAdView.addNewView(child, index);
+
+    }
+
+
+    @ReactProp(name = PROP_REFRESH_INTERVAL)
+    public void setRefreshInterval(final RNNativeAdWrapper view, final int interval) {
+
+        nativeAdView.setAdRefreshInterval(interval);
+    }
+
+    @ReactProp(name = PROP_MEDIA_VIEW)
+    public void setMediaView(final RNNativeAdWrapper view, final int mediaViewId) {
+
+        nativeAdView.addMediaView(mediaViewId);
+
+    }
+
+    @ReactProp(name = PROP_TEST_DEVICES)
+    public void setPropTestDevices(final RNNativeAdWrapper view, final ReadableArray testDevices) {
+        ReadableNativeArray nativeArray = (ReadableNativeArray) testDevices;
+        ArrayList<Object> list = nativeArray.toArrayList();
+
+        List<String> testDeviceIds = Arrays.asList(list.toArray(new String[list.size()]));
+        RequestConfiguration configuration =
+                new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+        MobileAds.setRequestConfiguration(configuration);
+    }
+
+
+
     @ReactProp(name = PROP_AD_UNIT_ID)
     public void setPropAdUnitId(final RNNativeAdWrapper view, final String adUnitId) {
 
-        admobAdUnitId = adUnitId;
         nativeAdView.setAdUnitId(adUnitId);
 
     }
