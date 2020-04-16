b8c3e7e66aa51b7e24d6c9f65e166d3f300564bb
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-16 10:37:45 +0500

    Add Wrapper to handle layout updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   requireNativeComponent,
+  View
 } from "react-native";
 import { NativeAdContext, nativeAdView } from "./context";
 
 const NativeAdView = (props) => {
   const [nativeAd, setNativeAd] = useState(null);
-  
+  const [forceRefresh, setForceRefresh] = useState(false);
   function updateAd(ad) {
     if (ad) {
       setNativeAd(ad)
     }
+  }
+
+  const _onAdFailedToLoad = (event) => {
+    if (props.onAdFailedToLoad) {
+      props.onAdFailedToLoad(event);
+    }
+  }
+
+  const _onAdLoaded = (event) => {
+    if (props.onAdLoaded)
+      props.onAdLoaded(event.nativeEvent);
+  }
+
+  const _onAdClicked = (event) => {
+    if (props.onAdClicked)
+      props.onAdClicked(event.nativeEvent);
+  }
+
+  const _onAdImpression = (event) => {
+    if (props.onAdImpression)
+      props.onAdImpression(event.nativeEvent);
+  }
+
+  const _onAdClosed = (event) => {
+    if (props.onAdClosed)
+      props.onAdClosed(event.nativeEvent);
+  }
+
+  const _onAdOpened = (event) => {
+    if (props.onAdOpened)
+      props.onAdOpened(event.nativeEvent);
+     
+  }
+
+  const _onUnifiedNativeAdLoaded = event => {
+ 
+    updateAd(event.nativeEvent)
+    setTimeout(() => {
+      setForceRefresh(!forceRefresh);
+      setForceRefresh(!forceRefresh);
+      setForceRefresh(!forceRefresh)
+    }, 0)
+    if (props.onUnifiedNativeAdLoaded) {
+      props.onUnifiedNativeAdLoaded(event.nativeEvent);
+    }
+  }
+
+  const _onAdLefApplication = (event) => {
+
+    if (props.onAdLeftApplication)
+      props.onAdLeftApplication(event.nativeEvent);
+
   }
 
   return (
@@ -18,15 +71,23 @@ const NativeAdView = (props) => {
       value={{ nativeAd, setNativeAd }}
     >
       <UnifiedNativeAdView
-        {...props}
         ref={nativeAdView}
-        onUnifiedNativeAdLoaded={event => {
-          updateAd(event.nativeEvent)
-          if (this.props.onUnifiedNativeAdLoaded) {
-            props.onUnifiedNativeAdLoaded(event);
-          }
-        }}
-      />
+        onAdLoaded={_onAdLoaded}
+        onAdFailedToLoad={_onAdFailedToLoad}
+        onAdClicked={_onAdClicked}
+        onAdLeftApplication={_onAdLefApplication}
+        onAdOpened={_onAdOpened}
+        onAdClosed={_onAdClosed}
+        onAdImpression={_onAdImpression}
+        style={props.style}
+        onUnifiedNativeAdLoaded={_onUnifiedNativeAdLoaded}
+        adUnitID="ca-app-pub-3940256099942544/1044960115"
+      >
+        
+          {props.children}
+   
+      </UnifiedNativeAdView>
+
     </NativeAdContext.Provider>
   );
 
