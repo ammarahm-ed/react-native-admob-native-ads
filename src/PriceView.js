0dc5064db3f7441c2dd17e58686733a1063a9f49
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-16 19:40:00 +0500

    add PriceView

---
+++ src/PriceView.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import {
+  Text,
+} from "react-native";
+import { NativeAdContext } from "./context";
+
+const PriceView = (props) => {
+  const { nativeAd, setNativeAd } = useContext(NativeAdContext);
+
+
+
+
+
+  return (
+    <Text
+      {...props}
+      nativeID="adPriceView"
+    >
+      {nativeAd ? nativeAd.price : null}
+    </Text>
+ );
+}
+
+export default PriceView;
