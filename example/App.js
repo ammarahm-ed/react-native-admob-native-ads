032f4a47706b8c5c9743c03915f64db4889d798d
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-17 22:20:57 +0500

    add a simple example

--- example/App.js
+++ example/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Dimensions, View, } from 'react-native';
-import { CallToActionView, HeadlineView, IconView, NativeAdView, StoreView, TaglineView } from 'react-native-admob-native-ads';
+import { View } from 'react-native';
+import NativeAdView, { CallToActionView, IconView,ImageView, HeadlineView, TaglineView, AdvertiserView, HeaderView, MediaView } from 'react-native-admob-native-ads';
 
 const App = () => {
   const [adLoaded, setAdLoaded] = useState(false);
@@ -9,9 +9,7 @@ const App = () => {
   };
 
   const _onAdLoaded = () => {
-
     console.log('Ad has loaded');
-
   };
 
   return (
@@ -19,82 +17,114 @@ const App = () => {
       style={{
         height: '100%',
         width: '100%',
-        backgroundColor: 'red'
-
-      }}
-    >
-      <NativeAdView
-        onAdLoaded={_onAdLoaded}
-        onAdFailedToLoad={_onAdFailedToLoad}
-        onAdClicked={() => {
-          //alert('clicked me hey');
-        }}
-        onUnifiedNativeAdLoaded={() => {
-          setAdLoaded(true);
-        }}
-        style={{
-          width: Dimensions.get('window').width,
-          height: 400,
-          backgroundColor: 'white'
-        }}
-        adUnitID="ca-app-pub-3940256099942544/2247696110"
-      >
-        <View
-          style={{
-            height: 400,
-            width: 400,
-            backgroundColor: "yellow"
+        marginTop:10
+      }}>
+      <View>
+        <NativeAdView
+          onAdLoaded={_onAdLoaded}
+          onAdFailedToLoad={_onAdFailedToLoad}
+          onAdClicked={() => {
+            //alert('clicked me hey');
           }}
-        >
-          <IconView
-            style={{
-              width: 100,
-              height: 100,
-              backgroundColor: 'black'
-            }}
-          />
-          <HeadlineView
-            style={{
-              height: 30,
-              width: '100%'
-            }}
-          />
-          <TaglineView
-            style={{
-              height: 50,
-              width: '100%'
-            }}
-          />
-          <CallToActionView
-            textStyle={{
-              color: 'white',
-              width: '100%',
-              height: '100%',
-              textAlign:'center',
-              textAlignVertical:"center"
-            }}
-            style={{
-              width: 80,
-              height: 50,
-              backgroundColor: 'red',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: 10
-
-            }}
-
-          />
-
-          <StoreView />
+          onUnifiedNativeAdLoaded={() => {
+            setAdLoaded(true);
+          }}
+          style={{
+            width: '95%',
+            alignSelf:'center',
+            height: 400,
+          }}
+          adUnitID="ca-app-pub-3940256099942544/2247696110">
           <View
             style={{
-              width: 100,
-              height: 50
-            }}
-          >
+              height: 400,
+              width: '100%',
+              backgroundColor: 'white',
+            }}>
+              <HeaderView/>
+            <View
+              style={{
+                height: 100,
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                paddingHorizontal:10
+              }}>
+              <IconView
+                style={{
+                  width: 70,
+                  height: 70,
+                }}
+              />
+                <View
+                style={{
+                  width:'75%',
+                  paddingHorizontal:6
+                }}
+                >
+                <HeadlineView
+                style={{
+                  fontWeight:"bold",
+                  fontSize:14,
+                }}
+
+                />
+                <TaglineView
+                numberOfLines={2}
+                  style={{
+                    fontSize:12,
+                  }}
+                />
+                <AdvertiserView
+                  style={{
+                    fontSize:10,
+                    color:"gray"
+                  }}
+                />
+              </View>
+             
+
+            </View>
+
+            <View
+              style={{
+                height: 200,
+                width: '100%',
+              }}>
+              <MediaView
+                style={{
+                  width: '90%',
+                  height: 200,
+                  alignSelf:'center',
+                  borderRadius:10
+                }}
+              />
+            
+            </View>
+
+            <CallToActionView
+              style={{
+                height: 45,
+                paddingHorizontal:12,
+                backgroundColor: 'purple',
+                justifyContent:'center',
+                alignItems:'center',
+                borderRadius:5,
+                elevation:10,
+                marginTop:10
+                
+              }}
+              textStyle={{color:'white',fontSize:16}}
+            
+            />
+
+           
+
+           
           </View>
-        </View>
-      </NativeAdView>
+        </NativeAdView>
+      </View>
     </View>
   );
 };
