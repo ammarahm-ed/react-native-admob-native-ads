7691ef5d7953119aa686808b0e5e8f9f23b2fd28
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-17 12:51:46 +0500

    add HeaderView

---
+++ src/HeaderView.js
@@ -0,0 +1,40 @@
+
+import React from "react";
+import {
+  View,
+  Text,
+} from "react-native";
+
+
+
+const HeaderView = (props) => {
+
+
+  return (
+    <View
+      style={[
+        {
+          width: '100%',
+          height: 18,
+          borderWidth: 1,
+          borderRadius: 2.5,
+          borderColor: 'green'
+        }, props.style
+      ]}
+    >
+      <Text
+        style={[
+          {
+            color: 'green'
+          },
+          props.textStyle
+        ]}
+      >
+        Ad
+      </Text>
+    </View>
+  );
+}
+
+
+export default HeaderView;
