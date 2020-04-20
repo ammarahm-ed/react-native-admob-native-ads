bec8f0563582956e5078cac8122153b0dfac705b
ammarahm-ed <ammarahmed6506@gmail.com>
2020-04-20 16:01:02 +0500

    update loadAds method in example

--- example/index.js
+++ example/index.js
@@ -7,5 +7,6 @@ import App from './App';
 import {name as appName} from './app.json';
 const AdsManger = NativeModules.RNAdmobNativeAdsManager;
 
-AdsManger.loadAds("ca-app-pub-3940256099942544/2247696110",5);
+AdsManger.loadAds("ca-app-pub-3940256099942544/2247696110",5,1000000);
+
 AppRegistry.registerComponent(appName, () => App);
