a1b1e30420af1e06ed12c8f6a7907632c3f3d90b
ammarahm-ed <ammarahmed6506@gmail.com>
2020-08-21 20:39:24 +0500

    clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Platform, requireNativeComponent } from "react-native";
 import BatchedBridge from "react-native/Libraries/BatchedBridge/BatchedBridge";
-import AdManager from "./AdManager";
 import { NativeAdContext } from "./context";
 import Wrapper from "./Wrapper";
 
