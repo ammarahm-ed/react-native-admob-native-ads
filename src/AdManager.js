7d308e6680ffc226cf8ade03ef93d06e3b9ce65c
Marc Rousavy <marcrousavy@hotmail.com>
2020-08-31 11:54:59 +0200

    Remove hardcoded type consts from AdManager.js

--- src/AdManager.js
+++ src/AdManager.js
@@ -2,37 +2,15 @@ import {NativeModules} from 'react-native';
 
 const RNAdmobNativeAdsManager = NativeModules.RNAdmobNativeAdsManager;
 
-const MAX_AD_CONTENT_RATING = {
-  G: "G",
-  MA:"MA",
-  PG: "PG",
-  T: "T",
-  UNSPECIFIED: "",
-}
-const TAG_FOR_CHILD_DIRECTED_TREATMENT = {
-  TRUE:1,
-  FALSE:0
-}
-
-const TAG_FOR_UNDER_AGE_CONSENT = {
-  TRUE:1,
-  FALSE:0
-}
-
 function setRequestConfiguration(config) {
-
   RNAdmobNativeAdsManager.setRequestConfiguration(config);
 }
 
-async function isTestDevice() {
-
-  return await RNAdmobNativeAdsManager.isTestDevice();
+function isTestDevice() {
+  return RNAdmobNativeAdsManager.isTestDevice();
 }
 
 export default {
   setRequestConfiguration,
-  isTestDevice,
-  MAX_AD_CONTENT_RATING,
-  TAG_FOR_CHILD_DIRECTED_TREATMENT,
-  TAG_FOR_UNDER_AGE_CONSENT
+  isTestDevice
 }
