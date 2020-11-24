7316f7d8cf84f1b9a3b453e283adab1a9a4ddae9
ammarahm-ed <ammarahmed6506@gmail.com>
2020-11-24 19:36:38 +0500

    refactor events into RNAdMobGlobals class

---
+++ android/src/main/java/com/ammarahmed/rnadmob/nativeads/RNAdMobGlobals.java
@@ -0,0 +1,15 @@
+package com.ammarahmed.rnadmob.nativeads;
+
+public class RNAdMobGlobals {
+
+    public static RNAdMobPreloader preloader = new RNAdMobPreloader();
+
+    public static final String EVENT_AD_FAILED_TO_LOAD = "onAdFailedToLoad";
+    public static final String EVENT_AD_CLICKED = "onAdClicked";
+    public static final String EVENT_AD_CLOSED = "onAdClosed";
+    public static final String EVENT_AD_OPENED = "onAdOpened";
+    public static final String EVENT_AD_IMPRESSION = "onAdImpression";
+    public static final String EVENT_AD_LOADED = "onAdLoaded";
+    public static final String EVENT_AD_LEFT_APPLICATION = "onAdLeftApplication";
+
+}
