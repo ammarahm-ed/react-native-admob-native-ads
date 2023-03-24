6bbe70b5a427a78bc18c391f6bda01585f203c9b
Cedric <cedric.bridges3@gmail.com>
2023-03-24 17:00:42 -0500

    Updated package.json.

--- example/post-script.js
+++ example/post-script.js
@@ -13,28 +13,6 @@ for (var i = 0; i < files.length; i++) {
                 console.error("could not execute command: ", err);
                 return
             }
-            fs.readFile('package.json', {encoding: 'utf-8'}, function(err, data) {
-                if (err) throw err;
-            
-                let dataArray = data.split('\n');
-                const searchKeyword = `"react-native-admob-native-ads": "file:..",`;
-                let lastIndex = -1;
-            
-                for (let index=0; index<dataArray.length; index++) {
-                if (dataArray[index].includes(searchKeyword)) {
-                        lastIndex = index;
-                        break; 
-                    }
-                }
-            
-                dataArray.splice(lastIndex, 1);
-            
-                const updatedData = dataArray.join('\n');
-                fs.writeFile('package.json', updatedData, (err) => {
-                    if (err) throw err;
-                    console.log ('Successfully updated the file data');
-                });
-            });
             console.log('Postinstall complete.');
         });
         break;
